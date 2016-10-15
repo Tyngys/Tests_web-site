dccefdf4f8813c5437b17e5a0b60e452878f8d03
SERGEY-LAPTOP\Admin <xakerzerg@gmail.com>
2016-10-15 18:09:40 +0300

    Change dispose in TesTSerbice

--- CP/CP.Business/TestService.cs
+++ CP/CP.Business/TestService.cs
@@ -33,8 +33,16 @@ namespace CP.Business
 
         public void Dispose()
         {
-            this.User?.Dispose();
-            this.UserRepository?.Dispose();
+            if (this.UserRepository != null)
+            {
+                this.UserRepository.Dispose();
+                
+            }
+            if (this.User != null)
+            {
+                this.User.Dispose();
+
+            }
         }
     }
 }
