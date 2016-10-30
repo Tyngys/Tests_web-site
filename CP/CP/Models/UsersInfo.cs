87f45185d8d6a96d9eeb3d053c9d7b30dca8b62a
SERGEY-LAPTOP\Admin <xakerzerg@gmail.com>
2016-10-30 16:17:44 +0300

    Add analiz tests

--- CP/CP/Models/UsersInfo.cs
+++ CP/CP/Models/UsersInfo.cs
@@ -8,18 +8,19 @@ namespace CP.Web.Models
 {
     public class UsersInfo
     {
-        public List<UserModel> InfoList { get; set; } = new List<UserModel>();
+        public List<User> InfoList { get; set; } = new List<User>();
 
-        public double[] ArrayAverg { get; set; } = new double[5];
-        public double[] ArrayArgvMiss { get; set; } = new double[5];
-        public double[] ArrayStErrorAvergMath { get; set; } = new double[5];
+        public double[] ArrayAverg { get; set; } = new double[6];
+        public double[] ArrayArgvMiss { get; set; } = new double[6];
+        public double[] ArrayStErrorAvergMath { get; set; } = new double[6];
 
         public UsersInfo(List<User> listUsers)
         {
-            foreach (User user in listUsers)
+            this.InfoList = listUsers;
+            /*foreach (User user in listUsers)
             {
                 this.InfoList.Add(new UserModel() {A_S = user.A_S, Age = user.Age, Gender = user.Gender, P_D = user.P_D, UserName = user.UserName, K = user.K, N_O = user.N_O, S_L = user.S_L});
-            }
+            }*/
            
             this.GetAverg(listUsers);
             this.GetAvergMiss(listUsers);
@@ -28,7 +29,7 @@ namespace CP.Web.Models
 
         public void GetAverg(List<User> listUser)
         {
-            double[] arraySum = new double[5];
+            double[] arraySum = new double[6];
             for (int i = 0; i < listUser.Count; i++)
             {
                 arraySum[0] += listUser[i].P_D;
@@ -36,14 +37,15 @@ namespace CP.Web.Models
                 arraySum[2] += listUser[i].N_O;
                 arraySum[3] += listUser[i].S_L;
                 arraySum[4] += listUser[i].K;
+                arraySum[5] += listUser[i].SecondTest.Mark;
             }
-
-
+            
             this.ArrayAverg[0] = arraySum[0] / listUser.Count;
             this.ArrayAverg[1] = arraySum[1] / listUser.Count;
             this.ArrayAverg[2] = arraySum[2] / listUser.Count;
             this.ArrayAverg[3] = arraySum[3] / listUser.Count;
             this.ArrayAverg[4] = arraySum[4] / listUser.Count;
+            this.ArrayAverg[5] = arraySum[5] / listUser.Count;
         }
 
         public void GetAvergMiss(List<User> listUser)
@@ -55,6 +57,7 @@ namespace CP.Web.Models
                 this.ArrayArgvMiss[2] += Math.Abs(listUser[i].N_O - this.ArrayAverg[2]) / listUser.Count;
                 this.ArrayArgvMiss[3] += Math.Abs(listUser[i].S_L - this.ArrayAverg[3]) / listUser.Count;
                 this.ArrayArgvMiss[4] += Math.Abs(listUser[i].K - this.ArrayAverg[4]) / listUser.Count;
+                this.ArrayArgvMiss[5] += Math.Abs(listUser[i].SecondTest.Mark - this.ArrayAverg[5]) / listUser.Count;
             }
         }
 
@@ -67,6 +70,7 @@ namespace CP.Web.Models
                 this.ArrayStErrorAvergMath[2] += Math.Sqrt(Math.Pow(listUser[i].N_O - this.ArrayAverg[2], 2) / listUser.Count);
                 this.ArrayStErrorAvergMath[3] += Math.Sqrt(Math.Pow(listUser[i].S_L - this.ArrayAverg[3], 2) / listUser.Count);
                 this.ArrayStErrorAvergMath[4] += Math.Sqrt(Math.Pow(listUser[i].K - this.ArrayAverg[4], 2) / listUser.Count);
+                this.ArrayStErrorAvergMath[5] += Math.Sqrt(Math.Pow(listUser[i].SecondTest.Mark - this.ArrayAverg[5], 2) / listUser.Count);
             }
         }
     }
--- FillUsers.sql
+++ FillUsers.sql
@@ -1,6 +1,6 @@
 declare @Name nvarchar(50),@pd int,@as int,@no int, @sl int, @k int,
 @Password nvarchar(50), @Age int,@i int,@RandomSymbol char(60),
-@RandomSymbolAndNumber char(70), @index int,@iglobal int,
+@RandomSymbolAndNumber char(70), @index int,@iglobal int,@mark int,
 @sex nvarchar(10),@stringNumber nvarchar(20),@numberPasport int,
 @Data2 Datetime,@y nvarchar(3),@n nvarchar(2)
 set @y='yes'
@@ -10,35 +10,42 @@ select @i=0,@index=0,@iglobal=0
 
 while @iglobal < 100
 begin
-          set @i=0
-	      set @Name=''
-		  set @Password=''
+          set @i = 0;
+	      set @Name='';
+		  set @Password='';
+		  set @Age = 0;
+		  set @pd =  0;
+		  set @as =  0;
+		  set @no =  0;
+		  set @sl =  0;
+		  set @k =  0;
+		  set @mark = 0;
 
 		  if @iglobal%2 =0
 		  begin
-          set @sex='Man'
+          set @sex='Man';
           end
           if @iglobal%2 !=0
           begin
-          set @sex = 'Woman'
+          set @sex = 'Woman';
           end
 
 
 
       while @i < 15
 	  begin
-	      set @index=Rand()*60
-	      set @Name=@Name + substring(@RandomSymbol,@index, 1)
-		  set @i= @i + 1
+	      set @index=Rand()*60;
+	      set @Name=@Name + substring(@RandomSymbol,@index, 1);
+		  set @i= @i + 1;
 	  end
 
-	  set @i = 0
+	  set @i = 0;
 
 	  while @i < 8
 	  begin
-	      set @index=Rand()*60
-	      set @Password=@Password + substring(@RandomSymbol,@index, 1)
-		  set @i=@i+1
+	      set @index=Rand()*60;
+	      set @Password=@Password + substring(@RandomSymbol,@index, 1);
+		  set @i=@i+1;
 	  end
 
 	  set @Age = FLOOR(RAND()*(21-18)+18);
@@ -47,8 +54,12 @@ begin
 	  set @no =  FLOOR(RAND()*(8-0)+0);
 	  set @sl =  FLOOR(RAND()*(8-0)+0);
 	  set @k =  FLOOR(RAND()*(8-0)+0);
+	  set @mark =  FLOOR(RAND()*(50-0)+0);
+	  
 
 	  begin
+	  insert into SecondTests(Mark)
+	  values(@mark)
 	  insert into Users(UserName, Gender, Age, Password, P_D, A_S, N_O, S_L, K, RoleId)
 	  values(@Name, @sex, @Age, @Password, @pd, @as, @no, @sl, @k,1)
 	  end
